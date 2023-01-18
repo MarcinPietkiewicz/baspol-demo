db8dc9b73f17fb088b0167703c7e3390993534d2
Marcin <marcin.p.dev@gmail.com>
2023-01-18 16:20:34 +0100

    Preliminary bootstrap of main screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
-import './App.scss'
+import "./App.scss";
+import { Container, Row, Col, Button, Stack } from "react-bootstrap";
 
 function App() {
-
   return (
     <div className="App">
-
-      <h1>Lista płac</h1>
-
+      <Container className="cont">
+        <Row className="bg-light text-dark">
+          <Col sm="8">
+            <h2>BASPOL DEMO</h2>
+          </Col>
+          <Col sm="4">Listy płac ver 0.1</Col>
+        </Row>
+        <Row>
+          <Stack gap={2} className="col-sm-4 mx-auto">
+            <p>Klient 1</p>
+            <p>Klient 2</p>
+          </Stack>
+        </Row>
+        <Row className="m-3">
+          <Stack gap={2} className="col-sm-4 mx-auto">
+            <Button variant="primary">Dodaj nowego klienta</Button>
+            <Button variant="outline-primary">Wyloguj</Button>
+          </Stack>
+        </Row>
+      </Container>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
