b420775c38f38463f8e2bb47bd07e75e4315ab36
Marcin <marcin.p.dev@gmail.com>
2023-01-23 17:19:42 +0100

    console log of current issues

--- src/App.jsx
+++ src/App.jsx
@@ -5,34 +5,33 @@ import { Container, Row, Col, Button, Stack, Modal, Form } from "react-bootstrap
 import { useAppDispatch, useAppSelector } from "./app/hooks";
 import { addClient } from "./features/clients/client-slice";
 
-
 function App() {
   const [newClientModalShow, setNewClientModalShow] = useState(false);
   const [clients, setClients] = useState([]);
 
-  const x = useAppSelector(state => state.clients);
+  const x = useAppSelector((state) => state.clients);
 
   useEffect(() => {
     fetch("db.json")
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
-        const x = data.clients.map(entry => entry);
-        console.log('x:');
+        const x = data.clients.map((entry) => entry);
+        console.log("x:");
         console.log(x);
-        x.map(entry => setClients([...clients, { firstName: entry.name, surname: entry.surname }]));
+        x.map((entry) => setClients([...clients, { firstName: entry.name, surname: entry.surname }]));
         // not working - don't know why?
-        useAppDispatch(addClient('pellowski', 'adam'))
-        useAppDispatch(addClient(x.name, x.surname))
+        useAppDispatch(addClient("pellowski", "adam"));
+        useAppDispatch(addClient(x.name, x.surname));
         return data;
       })
-      .then(setTimeout(data => {
-        console.log('after 2 secs...:');
-        console.log(clients)}, 2000));
+      .then(
+        setTimeout((data) => {
+          console.log("after 2 secs...:");
+          console.log(clients);
+        }, 2000)
+      );
   }, []);
-
-  
-
 
   return (
     <div className="App">
@@ -46,9 +45,7 @@ function App() {
         </Row>
         <Row>
           <Stack gap={2} className="col-sm-4 mx-auto p-2">
-            <div className="bg-light border p-2">
-              Klient 1 - ilość pracowników - 
-            </div>
+            <div className="bg-light border p-2">Klient 1 - ilość pracowników -</div>
             <Button
               variant="primary"
               onClick={() => {
@@ -68,11 +65,22 @@ function App() {
 
 export default App;
 
+function handleSubmit(event) {
+  event.preventDefault();
+  console.log(event);
+  setNewClientModalShow(false);
+}
+
+function addNewClient(name, surname) {
+  useAppDispatch(addClient(name, surname));
+  console.log("dispatch sent");
+}
+
 // Components in main view
 
 function AddNewClientModal(props) {
   return (
-    <Modal {...props} size="lg" aria-labelledby="contained-modal-title-vcenter" centered>
+    <Modal {...props} size="lg" aria-labelledby="contained-modal-title-vcenter" centered id="addNewClientModal">
       <Modal.Header closeButton>
         <Modal.Title id="contained-modal-title-vcenter">Dodaj nowego klienta</Modal.Title>
       </Modal.Header>
@@ -85,7 +93,7 @@ function AddNewClientModal(props) {
 
 function AddNewClientForm() {
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <Form.Group className="mb-3" controlId="formSurname">
         <Form.Label>Nazwisko / firma</Form.Label>
         <Form.Control type="text" placeholder="np. Kowalski" />
